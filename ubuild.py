6f6600a5ba59ae82b183237690619ad400244fa1
lokiwh <lokiwh@zillowgroup.com>
2017-03-21 12:15:27 -0700

    made ubuild.py not dependent on zillow internals

--- ubuild.py
+++ ubuild.py
@@ -1,6 +1,9 @@
-from uranium import current_build
+import os
+import sys
+import uranium
 
-current_build.config.set_defaults({
+
+uranium.current_build.config.set_defaults({
     "package_name": "setuptools-cmmi",
     "module": "setuptools_cmmi",
     "test_packages": {
@@ -8,10 +11,46 @@ current_build.config.set_defaults({
     },
 })
 
-current_build.include("https://stash.atl.zillow.net/projects/LIBS/repos/egg.zillow-uranium/browse/zillow_base.py?raw=true",
-                      cache=True)
-from zillow_uranium import setup_egg
-setup_egg(current_build)
+def main(build):
+    build.packages.install(".", develop=True)
+
+    for package, version in build.config.get("additional_packages", {}).items():
+        build.packages.install(package, version=version)
+
+
+def test_packages(build):
+    build.packages.versions.update({
+        "coverage": "==4.0.1"
+    })
+    build.packages.install("mock")
+    build.packages.install("pytest")
+    build.packages.install("pytest-cov")
+    build.packages.install("pytest-runfailed")
+    for pkg, version in build.config.get(
+            "test_packages", {}
+    ).items():
+        build.packages.install(pkg, version=version)
+
+
+@uranium.task_requires(test_packages)
+def test(build):
+    """ run unit tests with pytest. """
+    test_root = build.config.get(
+        "test_root", os.path.join(build.config.get("module"), "tests")
+    )
+    # we add our build/bin to the path.
+    build.executables.run([
+        sys.executable,
+        os.path.join(build.root, "bin", "py.test"),
+        test_root,
+        "--cov", build.config.get("module"),
+        "--cov-report", "term-missing"
+    ] + build.options.args)
+
+#current_build.include("https://stash.atl.zillow.net/projects/LIBS/repos/egg.zillow-uranium/browse/zillow_base.py?raw=true",
+#                      cache=True)
+#from zillow_uranium import setup_egg
+#setup_egg(current_build)
 # how do I customize? see:
 # https://stash.atl.zillow.net/projects/LIBS/repos/egg.zillow-uranium/browse
 # http://uranium.readthedocs.io/en/latest/
